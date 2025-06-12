d00b2e5b5e5a04f8eac9f69fcdf6c8f445c99a07
ihatemondais <ngoquangkhoi1508@gmail.com>
2025-06-12 22:59:35 +0700

    hashed pw, try catch

--- DataAccessLayer/DTO/RegisterDTO.cs
+++ DataAccessLayer/DTO/RegisterDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +9,16 @@ namespace DataAccessLayer.DTO
 {
     public class RegisterDTO
     {
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(16, ErrorMessage = "Username cannot be longer than 50 characters.",MinimumLength = 4)]
         public string Username { get; set; } = null!;
 
-        public string PasswordHash { get; set; } = null!;
 
+        public string PasswordHash { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
         public string? Email { get; set; }
         public string? NationalId { get; set; }
         public string? FullName { get; set; }
-        public int RoleId { get; set; }
-    }
+
+       }
 }
