97373a2d4c5f9b25c97b6535d3b82c61cd2a96d2
Huynh An Khuong <huynhankhuong0511@gmail.com>
2025-06-13 14:46:21 +0700

    Update Entities, Implement Registration API, Create and implement Timeslot Service Repo API

--- BusinessLayer/IService/IDonationRegistrationServices.cs
+++ BusinessLayer/IService/IDonationRegistrationServices.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataAccessLayer.DTO;
 using DataAccessLayer.Entity;
 
 namespace BusinessLayer.IService
 {
-    public interface IDonationRegistrationService
+    public interface IDonationRegistrationServices
     {
         Task<DonationRegistration> GetRegistrationByIdAsync(int registrationId);
         Task<IEnumerable<DonationRegistration>> GetAllRegistrationsAsync();
@@ -16,7 +17,7 @@ namespace BusinessLayer.IService
         Task<IEnumerable<DonationRegistration>> GetRegistrationsByStatusIdAsync(int statusId);
         Task<IEnumerable<DonationRegistration>> GetRegistrationsByQrCodeAsync(string qrCode);
         Task<IEnumerable<DonationRegistration>> GetRegistrationByTimeSlotIdAsync(int timeSlotId);
-        Task<DonationRegistration> AddRegistrationAsync(DonationRegistration registration);
+        Task AddRegistrationAsync(DonationRegistrationDTO registration);
         Task<DonationRegistration> UpdateRegistrationAsync(DonationRegistration registration);
         Task<bool> DeleteRegistrationAsync(int registrationId);
         Task<bool> UpdateRegistrationStatusAsync(int registrationId, int statusId);
--- DataAccessLayer/Entity/DonationSchedule.cs
+++ DataAccessLayer/Entity/DonationSchedule.cs
@@ -9,8 +9,6 @@ public partial class DonationSchedule
 
     public DateTime? ScheduleDate { get; set; }
 
-    public int? TotalSlots { get; set; }
-
     public int RegisteredSlots { get; set; }
 
     public DateTime CreatedAt { get; set; }
