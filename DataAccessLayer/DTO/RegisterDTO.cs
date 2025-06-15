e27ea258c2589cd1bcbb32b6be600f1dcf649ffb
Huynh An Khuong <huynhankhuong0511@gmail.com>
2025-06-15 21:43:12 +0700

    Add UserName to Register

--- DataAccessLayer/DTO/RegisterDTO.cs
+++ DataAccessLayer/DTO/RegisterDTO.cs
@@ -13,9 +13,9 @@ namespace DataAccessLayer.DTO
         public string? Email { get; set; }
         [Required(ErrorMessage = "Password is required.")]
         public string PasswordHash { get; set; } = null!;
-        //[Required(ErrorMessage = "Username is required.")]
+        [Required(ErrorMessage = "Username is required.")]
         //[StringLength(16, ErrorMessage = "Username cannot be longer than 16 characters.", MinimumLength = 4)]
-        //public string Username { get; set; } = null!;
+        public string Username { get; set; }
         //[Required(ErrorMessage = "Full Name is required.")]
         //public string? FullName { get; set; }
         //[Required(ErrorMessage = "Phone Number is required.")]
