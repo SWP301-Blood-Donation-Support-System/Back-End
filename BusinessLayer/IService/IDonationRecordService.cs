13438abd8cc3be5b0a76c1c88ab13a9327e46644
Huy2223 <sasukevsmatadi@gmail.com>
2025-06-19 00:06:58 +0700

    Revert "Delete BusinessLayer/IService/IDonationRecordService.cs"
    This reverts commit e0a28eb6bf7bf2c37748e23a5782f2878a0147a9.

---
+++ BusinessLayer/IService/IDonationRecordService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.DTO;
+using DataAccessLayer.Entity;
+
+namespace BusinessLayer.IService
+{
+    public interface IDonationRecordService
+    {
+        Task<IEnumerable<DonationRecord>> GetAllDonationRecordsAsync();
+        Task<DonationRecord> GetRecordsByIdAsync(int recordId);
+        Task AddRecordsAsync(DonationRecordDTO donationRecord);
+        Task<bool> UpdateRecordsAsync(DonationRecord record);
+        Task<bool> DeleteRecordsAsync(int recordId);
+        Task<IEnumerable<DonationRecord>> GetRecordsByRegistrationIdAsync(int registrationId);
+        Task<IEnumerable<DonationRecord>> GetRecordsByDonationDateTimeAsync(DateTime donationDateTime);
+        Task<IEnumerable<DonationRecord>> GetRecordsByDonationTypeIdAsync(int donationTypeId);
+        Task<bool> ValidateDonationRecordAsync(int recordId, int userId);
+        Task<bool> UpdateRecordByFieldsAsync(DonationRecordUpdateDTO updateDTO);
+        Task<IEnumerable<DonationRecord>> GetRecordsByResultAsync(int result);
+        Task<IEnumerable<DonationRecordDTO>> GetRecordsByUserId(int userId);
+        Task<bool> SaveChanges();
+        Task<IEnumerable<DonationValidation>> GetValidationsForRecordAsync(int recordId);
+        Task<IEnumerable<DonationRecord>> GetRecordsByValidatorAsync(int userId);
+    }
+}
